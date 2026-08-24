ba6b3077b913c9a6c58441567e8b2bad335e3088
Bruno Freitas <brunovctd3@gmail.com>
2026-08-23 22:02:28 -0300

    Melhorias

--- agente_acoes_nlp.py
+++ agente_acoes_nlp.py
@@ -88,7 +88,7 @@ class CLasse_agtacoes_nlp() :
                 val_max = max(sm)
                 lista_score.append({"ID":self.frame_chunk.loc[i,"ID"],"Valor":val_max})
         frame_score = pd.DataFrame(lista_score)
-        frame_score = frame_score.sort_values(by="Valor",ascending=False).reset_index(drop=True).head(20)
+        frame_score = frame_score.sort_values(by="Valor",ascending=False).reset_index(drop=True).head(30)
         frame_score_01 = pd.merge(frame_score,self.frame_chunk,left_on="ID",right_on="ID",how="left",suffixes=("_x","_y"))
         self.frame_scores = frame_score_01.copy()
         pprint.pprint(self.frame_scores)
@@ -107,8 +107,13 @@ class CLasse_agtacoes_nlp() :
                     val_text = f"""{val_text} {v_r}"""
                 lista_score.append({"ID":self.frame_scores.loc[i,"ID"],"Intensidade":len(lemma_noti.intersection(lemma_query)),"Retorno":val_text})
         frame_final = pd.DataFrame(lista_score)
-        frame_final_01 = pd.merge(frame_final,self.frame_tr,left_on="ID",right_on="ID_ref",how="left",suffixes=("_x","_y"))
-        frame_final_01 = frame_final_01.sort_values(by="Intensidade",ascending=False).reset_index(drop=True).head(9)
+        frame_final_01 = pd.merge(frame_final,self.frame_tr,left_on="ID",right_on="ID_ref",how="left",suffixes=("_x","_y")).head(20)
+        frame_final_01 = (
+            frame_final_01
+            .sample(n=9, random_state=42)
+            .reset_index(drop=True)
+        )
+        frame_final_01 = frame_final_01.sort_values(by="Intensidade",ascending=False).reset_index(drop=True)
         colunas_reff0 = ["Noticia","Data_ref","Tipo","Link"]
         frame_final_02 = frame_final_01[colunas_reff0]
         js_ret = frame_final_02.to_json(orient="records",force_ascii=False,indent=4)
